99fae7dd51872d45e7bd3c328639f193b7c28cc6
hansen9 <79235117+hansen9@users.noreply.github.com>
2024-07-05 09:53:39 +0700

    Richest customer wealth

---
+++ Easy/RichestCustomerWealth.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+
+using namespace std;
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int m = accounts.size();
+        int max;
+        int j = 0;
+        for(int i = 0; i < m; i++)
+        {
+            int n = accounts[i].size();
+            int curr = 0;
+            for(int j = 0; j < n; j++)
+            {
+                curr += accounts[i][j];
+            }
+
+            if(curr > max)
+            {
+                max = curr;
+            }
+        }
+        return max;
+    }
+};
