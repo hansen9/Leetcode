5173c43a5b7107969ebd6b5d50b0fcfbd43a0d41
hansen9 <79235117+hansen9@users.noreply.github.com>
2024-07-08 18:00:46 +0700

    Number of employee who met the target

---
+++ Easy/EmployeeTarget.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
+        int res = 0;
+
+        for(int i = 0; i < hours.size(); i++)
+        {
+            if(hours[i] >= target)
+            {
+                res++;
+            }
+        }
+        return res;
+    }
+};
