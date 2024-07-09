9bf4e39a3fa742e6e221a65d1c9f8803718be12b
hansen9 <79235117+hansen9@users.noreply.github.com>
2024-07-09 10:23:44 +0700

    Kids with the greatest number of candies

---
+++ Easy/MaxCandies.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int max = *max_element(candies.begin(), candies.end());
+        vector<bool> res(candies.size());
+
+        for(int i = 0; i < candies.size(); i++){
+            res[i] = ((candies[i] + extraCandies) >= max)? true : false;
+        }
+        return res;
+    }
+};
