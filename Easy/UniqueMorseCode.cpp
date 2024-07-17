6f7b94571a8650c78d9ad59115ecd85945768385
hansen9 <79235117+hansen9@users.noreply.github.com>
2024-07-17 14:10:36 +0700

    update

--- Easy/UniqueMorseCode.cpp
+++ Easy/UniqueMorseCode.cpp
@@ -1,20 +1,36 @@
 #include <vector>
 #include <unordered_set>
 #include <string>
+#include <map>
 
 using namespace std;
 class Solution {
 public:
     int uniqueMorseRepresentations(vector<string>& words) {
-        vector<string> d = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
-        unordered_set<string> s;
-        for(auto word : words){
-            string code;
-            for(auto character : word) code += d[character- 'a'];
-            s.insert(code);
-        }
-        return s.size();
+        string morse_codes[] = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        // using unordered_set -> can return the array of transformed words
+        // unordered_set<string> s;
+        // for(auto word : words){
+        //     string code;
+        //     for(auto character : word) code += morse_codes[character- 'a'];
+        //     s.insert(code);
+        // }
+        // return s.size();
 
+        // using map -> can't return the array of transformed word
+        int transformations = 0;
+        map<string, bool> vis;
+        for (string word : words) {
+            string morse_code = "";
+            for (char c : word) {
+                morse_code += morse_codes[c - 'a'];
+            }
+            if (!vis[morse_code]) {
+                transformations++;
+                vis[morse_code] = true;
+            }
+        }
+        return transformations;
         
     }
 };
